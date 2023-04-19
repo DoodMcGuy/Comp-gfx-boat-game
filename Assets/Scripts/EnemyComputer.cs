44e7521d794405dfa986716a6baabb5949d01dbb
IgnisPanem <thoefle2001@gmail.com>
2023-04-19 12:48:59 -0500

    Update EnemyComputer.cs

--- Assets/Scripts/EnemyComputer.cs
+++ Assets/Scripts/EnemyComputer.cs
@@ -27,7 +27,7 @@ public class EnemyComputer : MonoBehaviour
 
 	private void Awake()
     {
-		player = GameObject.Find("Boat").transform;
+		player = GameObject.Find("Longship").transform;
 		agent = GetComponent<NavMeshAgent>();
     }
 
