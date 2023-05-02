b448584daeffac905ea96cb865a3bf19f344d821
IgnisPanem <thoefle2001@gmail.com>
2023-05-02 17:32:47 -0500

    new lap counter added

---
+++ Assets/OneWayCollider.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+
+public class OneWayCollider : MonoBehaviour
+{
+    BoxCollider collider;
+
+    void Awake()
+    {
+        collider = GetComponent<BoxCollider>();
+        collider.isTrigger = true;
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject collidedObject = other.gameObject;
+
+        if (collidedObject.tag == "Boat")
+            collidedObject.GetComponent<Lap>().LapTime();
+
+        Lap collidedObjectLapScript = null;
+        if (collidedObject.tag == "Boat")
+            collidedObjectLapScript = collidedObject.GetComponent<Lap>();
+        else
+            return;
+
+        if (collidedObjectLapScript.getLapFlag() == true)
+            collidedObjectLapScript.LapTime();
+
+    }
+
+    void OnTrigerStay(Collider other)
+    {
+        GameObject collidedObject = other.gameObject;
+        Lap collidedObjectLapScript = null;
+        if (collidedObject.tag == "Boat")
+            collidedObjectLapScript = collidedObject.GetComponent<Lap>();
+        else
+            return;
+
+        if (collidedObjectLapScript.getLapFlag() == true)
+            Physics.IgnoreCollision(collider, other, true);
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        GameObject collidedObject = other.gameObject;
+        Lap collidedObjectLapScript = null;
+        if (collidedObject.tag == "Boat")
+            collidedObjectLapScript = collidedObject.GetComponent<Lap>();
+        else
+            return;
+
+        if (collidedObjectLapScript.getLapFlag() == true)
+            collidedObjectLapScript.switchLapFlag();
+    }
+}
--- Assets/Scripts/Lap.cs
+++ Assets/Scripts/Lap.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Lap : MonoBehaviour
 {
-    private int lapCount =0;
+    private int lapCount = 0;
+    private bool lapFlag = false;
     private List<System.DateTime> lapTimes = new List<System.DateTime>();
 
-    public void LapTime(){
+    public void LapTime()
+    {
         lapTimes.Add(System.DateTime.Now);
         lapCount++;
 
@@ -16,19 +18,32 @@ public class Lap : MonoBehaviour
         Debug.Log(System.DateTime.Now);
     }
 
-    public int GetLapCount(){
+    public int GetLapCount()
+    {
         return lapCount;
     }
 
-    public string GetFastestTime(){
-        
+    public void switchLapFlag()
+    {
+        lapFlag = !lapFlag;
+    }
+
+    public bool getLapFlag()
+    {
+        return lapFlag;
+    }
+
+    public string GetFastestTime()
+    {
+
         var minTime = new System.TimeSpan();
         var deltaTime = new System.TimeSpan(9999, 9999, 9999);
-        
-        for(int i =0; i < lapTimes.Count - 1; i++){
-            deltaTime = lapTimes[i+1] - lapTimes[i];
 
-            if(minTime > deltaTime)
+        for (int i = 0; i < lapTimes.Count - 1; i++)
+        {
+            deltaTime = lapTimes[i + 1] - lapTimes[i];
+
+            if (minTime > deltaTime)
                 minTime = deltaTime;
         }
 
